6a0a87c350d8f8904036ea58a681c3e417122185
kinbara <kinsen_g@icloud.com>
2019-10-07 15:14:33 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-    root 'static_pages#top'
-    get '/signup', to: 'users#new'
-    resources :users
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
